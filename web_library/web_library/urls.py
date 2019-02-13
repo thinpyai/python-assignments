1bf0ae0709849ba3300362ca162b1916145502b5
thinpyai <thinpyaiwin@gmail.com>
2019-02-14 00:40:17 +0630

    Add files via upload
    Update urls.py by removing url file referencing

--- web_library/web_library/urls.py
+++ web_library/web_library/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from book import views
 
 urlpatterns = [
-    path('', include('web_library.main_urls')),
+    path('', views.index, name='index'),
     path('', include('django.contrib.auth.urls')),
     path('book/', include('book.urls')),
     path('admin/', admin.site.urls),
