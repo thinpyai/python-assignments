b461afc323ebd37ae37003e2a8e489066d956216
thinpyai <thinpyaiwin@gmail.com>
2019-02-14 00:27:37 +0630

    Add files via upload
    Updating settings file to include setting for login page

--- web_library/web_library/settings.py
+++ web_library/web_library/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'web_library.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -67,6 +67,8 @@ TEMPLATES = [
         },
     },
 ]
+
+LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'web_library.wsgi.application'
 
