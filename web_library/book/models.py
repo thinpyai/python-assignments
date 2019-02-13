93884084eefd4a83f11a41477037ebb312ca3d74
thinpyai <thinpyaiwin@gmail.com>
2019-02-14 00:30:46 +0630

    Add files via upload
    Update book model

--- web_library/book/models.py
+++ web_library/book/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
 class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
-    publisher = models.CharField(max_length=200)
+    title = models.CharField(max_length=64)
+    author = models.CharField(max_length=20)
+    publisher = models.CharField(max_length=20)
+    rentalStatus = models.IntegerField
+    summary = models.CharField(max_length=200)
+    releaseDate = models.DateField
+    category = models.CharField(max_length=20)
     pub_date = models.DateTimeField('date published')
     def __str__(self):
         return self.title
