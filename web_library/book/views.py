70b5d7447a60d75f3c0adc10c4bf95fa16f1b206
thinpyai <thinpyaiwin@gmail.com>
2019-02-18 00:27:48 +0630

    Add files via upload
    Add method for borrow

--- web_library/book/views.py
+++ web_library/book/views.py
@@ -13,21 +13,24 @@ from rest_framework import viewsets
 from django.contrib.auth.decorators import login_required
 
 # @login_required
+
+
 def index(request):
     if request.user.is_authenticated:
         return render(request, 'book/home.html')
     else:
         return redirect('/login/')
 
+
 @login_required
 def book(request):
     # return render(request, 'book/bookList.html')
-    
+
     # json = serializers.serialize('json', book_list)
     # return HttpResponse(template.render(json, request))
 
     # return HttpResponse(json, content_type='application/json')
-    book_list = Book.objects.all() 
+    book_list = Book.objects.all()
     template = loader.get_template('book/bookList.html')
     context = {
         'book_list': book_list,
@@ -35,13 +38,16 @@ def book(request):
     return HttpResponse(template.render(context, request))
 
 # to call when datatable is used.
+
+
 class BookListViewSet(viewsets.ModelViewSet):
-    book_list = Book.objects.all() 
+    book_list = Book.objects.all()
     serializer_class = BookSerializer
 
     def list(self, request):
         serializer = self.serializer_class(self.queryset, many=True)
         return Response(serializer.data)
+
 
 @login_required
 def add(request):
@@ -49,32 +55,31 @@ def add(request):
         form = BookForm(request.POST)
         if form.is_valid():
             book = form.save(commit=False)
-            #to modify in later
+            # to modify in later
             book.release_date = timezone.now()
-            book.rental_status = 0 # 0 is available to rent, 1 is already rent and unavailable to rent again
+            # 0 is available to rent, 1 is already rent and unavailable to rent again
+            book.rental_status = 0
             book.save()
             return redirect('book')
     else:
         form = BookForm()
     return render(request, 'book/addBook.html', {'form': form})
 
+
 @login_required
 def edit(request, book_id):
     book = get_object_or_404(Book, id=book_id)
-    if request.method == "POST":
-        form = BookForm(request.POST,instance=book)
+    if (book.rental_status == 0):
+        if request.method == "POST":
+            form = BookForm(request.POST, instance=book)
         if form.is_valid():
             book = form.save(commit=False)
-            #to modify in later
-            # book.release_date = timezone.now()
-            # book.rental_status = 0 # 0 is available to rent, 1 is already rent and unavailable to rent again
             book.save()
             return redirect('book')
-    else:
-        form = BookForm(instance=book)
+        else:
+            form = BookForm(instance=book)
     return render(request, 'book/editBook.html', {'form': form})
-    # response = "You're looking at the results of book %s."
-    # return HttpResponse(response % book_id)
+
 
 @login_required
 def delete(request, book_id):
@@ -85,7 +90,38 @@ def delete(request, book_id):
         form = BookForm(instance=book)
     return redirect('book')
 
+
 @login_required
 def borrow(request, book_id):
-    response = "You're looking at the results of book %s to borrow."
-    return HttpResponse(response % book_id)
+    book = get_object_or_404(Book, id=book_id)
+    if (book.rental_status == 0):
+        # Change to show available
+        book.rental_status = 1
+        status = "Available"
+        btn_text = "Return"
+    else:
+        # Change to show on-loan
+        book.rental_status = 0
+        status = "On Loan"
+        btn_text = "Borrow"
+
+    template = loader.get_template('book/borrowBook.html')
+    context = {
+        'title': book.title,
+        'status' : status,
+        'btn_text' : btn_text
+    }
+    return HttpResponse(template.render(context, request))
+    # template_name = 'polls/detail.html'
+    # book = get_object_or_404(Book, id=book_id)
+    
+    # form = BookForm(request.GET, instance=book) 
+    # if form.is_valid():
+    #     book = form.save(commit=False)
+    #     book
+    #     # update foreign key
+    #     book.save()
+    #     return redirect('book')
+    # else:
+    #     form = BookForm(instance=book)
+    # return render(request, 'book/borrowBook.html', {'form': form})
