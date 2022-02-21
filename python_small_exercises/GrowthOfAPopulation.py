52498519f9541f319a31d456f10836606b76b20a
thinpyai <thinpyaiwin@gmail.com>
2022-02-22 00:18:49 +0630

    Add files via upload
    Upgrade answer

--- python_small_exercises/GrowthOfAPopulation.py
+++ python_small_exercises/GrowthOfAPopulation.py
@@ -31,6 +31,11 @@ def nb_year(p0: int, percent: float, aug: int, p: int):
             
     return year
 
+def nb_year_2(p0, percent, aug, p, years = 0):
+    if p0 < p:
+        return nb_year(p0 + int(p0 * percent / 100) + aug, percent, aug, p, years + 1)
+    return years
+
 if __name__ == "__main__":
     # print(nb_year(1500, 5, 100, 5000)) # 15
     # print(nb_year(1500000, 2.5, 10000, 2000000)) # 10
