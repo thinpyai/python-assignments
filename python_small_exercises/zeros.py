8f975808e8b3e0eb8afc3f0d18c960657ade49cc
thinpyai <thinpyaiwin@gmail.com>
2022-04-28 23:50:22 +0630

    Add files via upload

--- python_small_exercises/zeros.py
+++ python_small_exercises/zeros.py
@@ -16,14 +16,16 @@
 # # 12! = 479001600 --> 2 trailing zeros
 
 # 1000000000 must be finished within 12000 ms
+from ast import operator
 from unittest import result
 
 
 def zeros(n):
-    count_5 = count(n,5)
+    count_5 = count_1(n,5)
     return count_5
 
-def count(n,t):
+# For is slower than while
+def count_1(n,t):
     result = 0
     for i in range(t,n+1,t):
         while i>=t:
@@ -34,16 +36,54 @@ def count(n,t):
                 break
     return result
 
-def multiply(n):
+# Performance NG
+def count_2(n,t):
+    result = 0
+    i = t
+    while i in range(t,n+1):
+        j=i
+        while j>=t:
+            if j%t==0:
+                j=j//t
+                result+=1
+            else:
+                break
+        i+=t
+    return result
+
+# --------------    
+
+def multiply_1(n):
     result = 1
-    for i in range(1,n+1):
+    i = 2
+    for i in range(2,n+1):
         result=result*i
     return result
+
+
+def multiply_2(n):
+    result = 1
+    i = 2
+    while i in range(2,n+1):
+        result*=i
+        i+=1
+    return result
+
+# -------------- 
+
+import math
+
+def zeros(n):
+    multiply_result = math.factorial(n)
+    multiply_str = str(multiply_result)
+    result = len(multiply_str) - len(multiply_str.rstrip('0'))
+    return result
+
 
 if __name__ == "__main__":
     # print(zeros(0)) #0
 
-    # print(multiply(5))
+    # print(multiply(5)) 
     # print(zeros(5)) #1
 
     # print(multiply(10))
@@ -76,7 +116,7 @@ if __name__ == "__main__":
     # print(multiply(50000))
     # print(zeros(50000))  #12499
 
-    print(multiply(1000000))
+    # print(multiply(1000000))
     print(zeros(1000000)) 
     
     # print(multiply(1000000000))
