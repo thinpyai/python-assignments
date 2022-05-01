8603ae20814139361c4a8d455b1bbe293cfd8152
thinpyai <thinpyaiwin@gmail.com>
2022-05-01 21:05:43 +0630

    Add files via upload

--- python_small_exercises/zeros.py
+++ python_small_exercises/zeros.py
@@ -16,108 +16,90 @@
 # # 12! = 479001600 --> 2 trailing zeros
 
 # 1000000000 must be finished within 12000 ms
-from ast import operator
-from unittest import result
+# Hint: You're not meant to calculate the factorial. Find another way to find the number of zeros.
 
+# Ans
+# Trailing 0s in n! = Count of 5s in prime factors of n!
+
+import math 
 
 def zeros(n):
-    count_5 = count_1(n,5)
-    return count_5
-
-# For is slower than while
-def count_1(n,t):
+    if n < 0:
+        return -1
+    i=5
     result = 0
-    for i in range(t,n+1,t):
-        while i>=t:
-            if i%t==0:
-                i=i//t
-                result+=1
-            else:
-                break
+    while n/i >= 1:
+    # for i in range(i, n/i >= 1, i):
+        result += n//i   #   = (n/5) + (n/25) + (n/125) + ....
+        i*=5
+
     return result
+
+# Others OK answers
+def zeros_1(n):
+  x = n/5
+  return x+zeros_1(x) if x else 0
+
+def zeros_2(n):
+    res = 0
+    while n >= 5:
+        res += n // 5
+        n //= 5
+    
+    return res
+ 
+def zeros_3(n):
+  if (n >= 5):
+      return math.floor(n / 5) + zeros_3(n / 5)
+  else:
+      return 0
+
+import itertools
+
+def zeros_4(n):
+    powers_of_5 = itertools.takewhile(lambda x: n/x > 0, (5**x for x in itertools.count(1)))
+
+    return sum(n / x for x in powers_of_5)
 
 # Performance NG
-def count_2(n,t):
-    result = 0
-    i = t
-    while i in range(t,n+1):
-        j=i
-        while j>=t:
-            if j%t==0:
-                j=j//t
-                result+=1
-            else:
-                break
-        i+=t
-    return result
-
-# --------------    
-
-def multiply_1(n):
-    result = 1
-    i = 2
-    for i in range(2,n+1):
-        result=result*i
-    return result
+# def zeros_old(n):
+#     result = 0
+#     for i in range(t,n+1,t):
+#         while i>=t:
+#             if i%t==0:
+#                 i=i//t
+#                 result+=1
+#             else:
+#                 break
+#     return result
 
 
-def multiply_2(n):
-    result = 1
-    i = 2
-    while i in range(2,n+1):
-        result*=i
-        i+=1
-    return result
+# Performance NG
+# def zeros(n):
+#     multiply_result = math.factorial(n)
+#     multiply_str = str(multiply_result)
+#     result = len(multiply_str) - len(multiply_str.rstrip('0'))
+#     return result
 
-# -------------- 
-
-import math
-
-def zeros(n):
-    multiply_result = math.factorial(n)
-    multiply_str = str(multiply_result)
-    result = len(multiply_str) - len(multiply_str.rstrip('0'))
-    return result
 
 
 if __name__ == "__main__":
     # print(zeros(0)) #0
-
-    # print(multiply(5)) 
+    # print(zeros(4)) #0
     # print(zeros(5)) #1
-
-    # print(multiply(10))
+    # print(zeros(9)) #1
     # print(zeros(10)) #2
-
-    # print(multiply(15))
     # print(zeros(15)) #3
-    
-    # print(multiply(6))
-    # print(zeros(6)) #1
-
-    # print(multiply(12))
-    # print(zeros(12)) #2
-
-    # print(multiply(18))
-    # print(zeros(18)) #3
-
-    # print(multiply(30))
-    # print(zeros(30)) #7
-
-    # print(multiply(1000))
-    # print(zeros(1000)) #249
-
-    # print(multiply(10000))
+    # print(zeros(19)) #3
+    # print(zeros(75)) #18
+    # print(zeros(100)) #24
+    # print(zeros_3(245)) #48
+    # print(zeros_3(1000)) #249
     # print(zeros(10000))  #2499
-
-    # print(multiply(100000))
     # print(zeros(100000))  #24999
-
-    # print(multiply(50000))
     # print(zeros(50000))  #12499
+    # print(zeros(1000000)) 
+    # print(zeros(1000000000)) #249999998
 
-    # print(multiply(1000000))
-    print(zeros(1000000)) 
-    
-    # print(multiply(1000000000))
-    # print(zeros(1000000000))  #24999
+
+
